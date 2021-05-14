c6128f548bafb4d67fcd3b3f226aa37ca1c3a910
Emixeras <felix.hagenbrock@posteo.de>
2021-05-14 19:26:17 +0200

    improved model and testing

--- src/train_rnn.py
+++ src/train_rnn.py
@@ -1,57 +1,95 @@
 import pandas as pd
-import sklearn.metrics
-from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
-df = pd.read_csv("../input/GME.csv")
-
-train_data = df.iloc[:70, 4:5].values
-test_data = df.iloc[70:, 4:5].values
-
-normalizer = MinMaxScaler(feature_range=(0, 1))
-train_data_normalized = normalizer.fit_transform(train_data)
-test_data_normalized = normalizer.fit_transform(test_data)
-
-X_train = []
-y_train = []
-for i in range(5, 70):
-    X_train.append(train_data_normalized[i - 5:i, 0])
-    y_train.append(train_data_normalized[i, 0])
-X_train = np.array(X_train)
-y_train = np.array(y_train)
-
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+from sklearn import metrics
+from sklearn.preprocessing import MinMaxScaler
 
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 
-model = Sequential()
+train_threshold = 60
+batch_size = 10
+neurons = 15
+epochs = 80
+hidden_layers = 1
 
-# input layer
-model.add(LSTM(units=10, return_sequences=False, input_shape=(X_train.shape[1], 1)))
-model.add(Dropout(0.2))
-
-# hidden layer
-model.add(LSTM(units=10, return_sequences=False))
-model.add(Dropout(0.2))
+# beste momentan 1 hidden 15 neurons 80 epochs (35) oder aber 1 hidden 15 neurons batch size 10 epochs =200
 
 
-# output layer
-model.add(Dense(units=1))
+def build_model(number_hidden_layers, number_neurons, number_epochs):
+    # build model
+    model = Sequential()
 
-model.compile(optimizer="adam", loss="mean_absolute_percentage_error")
-model.fit(X_train, y_train, epochs=100, batch_size=32)
-model.loss
+    for i in range(1, number_hidden_layers+1):
+        if i == number_hidden_layers and number_hidden_layers == 1:
+            model.add(LSTM(units=number_neurons, return_sequences=False, input_shape=(X_train.shape[1], 1)))
+            model.add(Dropout(0.2))
+        elif i == 1:
+            model.add(LSTM(units=number_neurons, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+            model.add(Dropout(0.2))
+        elif i == number_hidden_layers:
+            model.add(LSTM(units=number_neurons, return_sequences=False))
+            model.add(Dropout(0.2))
+        else:
+            model.add(LSTM(units=number_neurons, return_sequences=True))
+            model.add(Dropout(0.2))
 
+    # output layer
+    model.add(Dense(units=1))
+
+    model.compile(optimizer="adam", loss="mean_absolute_percentage_error")
+    model.fit(X_train, y_train, epochs=number_epochs, batch_size=1)
+    return model
+
+
+# read in csv and divide data into train and test
+df = pd.read_csv("../input/GME.csv")
+train_data = df.iloc[:train_threshold, 4:5].values
+test_data = df.iloc[train_threshold:, 4:5].values
+
+# normalize data
+normalizer = MinMaxScaler(feature_range=(0, 1))
+train_data_normalized = normalizer.fit_transform(train_data)
+test_data_normalized = normalizer.fit_transform(test_data)
+
+# transform data for input in model
+X_train = []
+y_train = []
+for i in range(batch_size, train_threshold):
+    X_train.append(train_data_normalized[i - batch_size:i, 0:1])
+    y_train.append(train_data_normalized[i, 0:1])
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+# prepare test data for prediction
 X_test = []
-for i in range(5, 19):
-    X_test.append(test_data_normalized[i-5:i, 0])
+inputs = df.iloc[:, 4:5]
+inputs = inputs[len(inputs)-len(test_data)-batch_size:].values
+inputs = inputs.reshape(-1, 1)
+inputs = normalizer.transform(inputs)
+
+for i in range(batch_size, len(inputs)):
+    X_test.append(inputs[i - batch_size:i, 0])
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+model = build_model(hidden_layers, neurons, epochs)
 
 predicted_stock_price = model.predict(X_test)
 
 predicted_stock_price = normalizer.inverse_transform(predicted_stock_price)
 
+print("MAE:", metrics.mean_absolute_error(test_data, predicted_stock_price))
+print("MAPE:", metrics.mean_absolute_percentage_error(test_data, predicted_stock_price) * 100)
+
+
+def mda(actual: np.ndarray, predicted: np.ndarray):
+    """ Mean Directional Accuracy """
+    return np.mean((np.sign(actual[1:] - actual[:-1]) == np.sign(predicted[1:] - predicted[:-1])).astype(int))
+
+
+print("MDA", mda(test_data, predicted_stock_price) * 100)
